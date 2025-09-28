f1c51e9dfccaed13c6421ec26335c6f547bf8a92
ambuila106 <brandon.ambuila@unillanos.edu.co>
2025-09-28 18:48:56 -0500

    initial and point tab

---
+++ lib/main.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:penka/widgets/score_card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Penka',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.grey[900], // 👈 Fondo global
+      ),
+      home: const MyHomePage(title: 'Penka'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        toolbarHeight: 70,
+        backgroundColor: Colors.black,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Image.asset(
+          'assets/logo-penka.png',
+          height: 40, // controla el tamaño
+        ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              ScoreCard(
+                leftTeamSrc: 'assets/arg.png',
+                rightTeamSrc: 'assets/bra.png',
+                leftScore: 2,
+                rightScore: 1,
+              ),
+              Divider(
+                height: 10,
+                color: Colors.grey[600],
+              ),
+              ScoreCard(
+                leftTeamSrc: 'assets/col.png',
+                rightTeamSrc: 'assets/chi.png',
+                leftScore: 3,
+                rightScore: 1,
+              ),
+              Divider(
+                height: 10,
+                color: Colors.grey[600],
+              ),
+              ScoreCard(
+                leftTeamSrc: 'assets/col.png',
+                rightTeamSrc: 'assets/chi.png',
+                leftScore: 3,
+                rightScore: 1,
+              ),
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+          ],
+        ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/widgets/score_card.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class ScoreCard extends StatelessWidget {
+  final String leftTeamSrc;
+  final String rightTeamSrc;
+  final int leftScore;
+  final int rightScore;
+
+  const ScoreCard({
+    Key? key,
+    required this.leftTeamSrc,
+    required this.rightTeamSrc,
+    required this.leftScore,
+    required this.rightScore,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.grey[900],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(leftTeamSrc, width: 70),
+            const SizedBox(width: 20),
+            _buildScoreBox(leftScore),
+            SizedBox(
+              width: 30,
+              child: Text(
+                textAlign: TextAlign.center,  
+                'vs',
+                style: TextStyle(color: Colors.white, fontSize: 12)
+              ),
+            ),
+
+            _buildScoreBox(rightScore),
+            const SizedBox(width: 20),
+            Image.asset(rightTeamSrc, width: 70),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 🔧 Método privado para no repetir el container de los puntajes
+  Widget _buildScoreBox(int score) {
+    return Container(
+      width: 50,
+      height: 53,
+      decoration: BoxDecoration(
+        color: Colors.deepPurple,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Center(
+        child: Text(
+          '$score',
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+Widget _buildInputScore(int score) {
+    return SizedBox(
+      width: 50,
+      height: 53,
+      child: TextField(
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+        decoration: InputDecoration(
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: Colors.deepPurple, // borde cuando no está enfocado
+              width: 1.5,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
